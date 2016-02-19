90a2ba0c6ecd0f2e911fc40aaa57e65af4563ee7
Jurie Horneman <jurie@jurie.org>
2016-02-19 17:37:27 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Choba-parser
+
+Choba (short for CHOice BAsed) is an experimental interactive storytelling engine written in JavaScript. It is intended to be used for choice-based interactive fiction (IF), and more.
+
+This is choba-parser, which takes input from a game designer or writer, and turns it into the data structures choba-engine expects.
---
+++ src/buildParser.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// Get and verify command line parameters.
+let opts = require("nomnom").parse();
+
+let grammarPath = opts[0];
+let outputPath = opts[1];
+
+if (!grammarPath || !outputPath) {
+    console.log("You must provide a path to the grammar and a path to the output file.");
+    process.exit(1);
+}
+
+let fs = require('fs');
+let path = require('path');
+
+// Load grammar file.
+grammarPath = path.normalize(grammarPath);
+let grammar = fs.readFileSync(grammarPath, 'utf8');
+
+console.log('Loaded grammar file...');
+
+// Build parser.
+let jison = require('jison');
+let parser = new jison.Parser(grammar);
+let parserSource = parser.generate();   // Can pass options but default is CommonJS.
+
+console.log('Built parser...');
+
+outputPath = path.normalize(outputPath);
+fs.writeFileSync(outputPath, parserSource, 'utf8');
+
+console.log('Saved parser.');
